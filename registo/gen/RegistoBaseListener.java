d75c1295da856e67d1e7946e295694658e45dd70
FilipaPereira <36512371+FilipaPereira@users.noreply.github.com>
2019-12-30 19:29:31 +0000

    Revert "visitor to turn register to json"
    This reverts commit 2dfb0e404668c553e401c1c769b3a7dc86ab5c18.

--- registo/gen/RegistoBaseListener.java
+++ registo/gen/RegistoBaseListener.java
@@ -1,4 +1,4 @@
-// Generated from C:/Users/Asus/Desktop/Registo/src\Registo.g4 by ANTLR 4.7.2
+// Generated from C:/Users/Asus/Desktop/registo/src\Registo.g4 by ANTLR 4.7.2
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
@@ -118,18 +118,6 @@ public class RegistoBaseListener implements RegistoListener {
 	 * <p>The default implementation does nothing.</p>
 	 */
 	@Override public void exitLocation(RegistoParser.LocationContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterPassword(RegistoParser.PasswordContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitPassword(RegistoParser.PasswordContext ctx) { }
 
 	/**
 	 * {@inheritDoc}
--- registo/gen/RegistoVisitor.java
+++ registo/gen/RegistoVisitor.java
@@ -1,4 +1,4 @@
-// Generated from C:/Users/Asus/Desktop/Registo/src\Registo.g4 by ANTLR 4.7.2
+// Generated from C:/Users/Asus/Desktop/registo/src\Registo.g4 by ANTLR 4.7.2
 import org.antlr.v4.runtime.tree.ParseTreeVisitor;
 
 /**
@@ -63,10 +63,4 @@ public interface RegistoVisitor<T> extends ParseTreeVisitor<T> {
 	 * @return the visitor result
 	 */
 	T visitLocation(RegistoParser.LocationContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link RegistoParser#password}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitPassword(RegistoParser.PasswordContext ctx);
 }
